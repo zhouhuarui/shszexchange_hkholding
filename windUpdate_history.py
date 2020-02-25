9c29f7aad345de1bc1d790e04a9d07d9b9c0fa1d
Huarui ZHOU <zhouhuarui@gmail.com>
2020-02-25 17:18:08 +0800

    Update windUpdate_history.py
    更新万得更新代码

--- windUpdate_history.py
+++ windUpdate_history.py
@@ -23,50 +23,52 @@ for updateDate in  dateList:
     
     
     #沪股通
-    dailyDataSH = w.wset("shstockholdings","date=%s" % updateDate,
-                         "field=wind_code,hold_stocks,publish_ratio,calculate_ratio,float_sharesratio")
-    dailyDataSH = pd.DataFrame(np.array(dailyDataSH.Data).T, 
-                               columns=["wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"])
-    dailyDataSH = dailyDataSH.astype({"wind_code" : str, "hold_stocks" : float, "publish_ratio" : float, 
-                                      "calculate_ratio" : float, "float_sharesratio" : float})
-    dailyDataSH["wind_code"] = dailyDataSH["wind_code"].str.slice(0, 6)
+    try:
+        dailyDataSH = w.wset("shstockholdings","date=%s" % updateDate,
+                             "field=wind_code,hold_stocks,publish_ratio,calculate_ratio,float_sharesratio")
+        dailyDataSH = pd.DataFrame(np.array(dailyDataSH.Data).T, 
+                                   columns=["wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"])
+        dailyDataSH = dailyDataSH.astype({"wind_code" : str, "hold_stocks" : float, "publish_ratio" : float, 
+                                          "calculate_ratio" : float, "float_sharesratio" : float})
+        dailyDataSH["wind_code"] = dailyDataSH["wind_code"].str.slice(0, 6)
+        
+        
+        #深股通
+        dailyDataSZ = w.wset("szstockholdings","date=%s" % updateDate,
+                             "field=wind_code,hold_stocks,publish_ratio,calculate_ratio,float_sharesratio")
+        dailyDataSZ = pd.DataFrame(np.array(dailyDataSZ.Data).T, 
+                                   columns=["wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"])
+        dailyDataSZ = dailyDataSZ.astype({"wind_code" : str, "hold_stocks" : float, "publish_ratio" : float, 
+                                          "calculate_ratio" : float, "float_sharesratio" : float})
+        dailyDataSZ["wind_code"] = dailyDataSZ["wind_code"].str.slice(0, 6)
+        
+        dailyData = dailyDataSH.append(dailyDataSZ)
+        dailyData["dataDate"] = updateDate
     
-    
-    #深股通
-    dailyDataSZ = w.wset("szstockholdings","date=%s" % updateDate,
-                         "field=wind_code,hold_stocks,publish_ratio,calculate_ratio,float_sharesratio")
-    dailyDataSZ = pd.DataFrame(np.array(dailyDataSZ.Data).T, 
-                               columns=["wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"])
-    dailyDataSZ = dailyDataSZ.astype({"wind_code" : str, "hold_stocks" : float, "publish_ratio" : float, 
-                                      "calculate_ratio" : float, "float_sharesratio" : float})
-    dailyDataSZ["wind_code"] = dailyDataSZ["wind_code"].str.slice(0, 6)
-    
-    dailyData = dailyDataSH.append(dailyDataSZ)
-    dailyData["dataDate"] = updateDate
-    
+    except:
+        
+        print ("There is no data for date:%s"  % updateDate)
+        continue
     
     sqlCodeDelete = "DELETE FROM WIND_DATA_DAILY WHERE DATADATE='%s';" % updateDate
     sqlCodeInsert = "INSERT INTO WIND_DATA_DAILY VALUES (" + ",".join(["%s"] * dailyData.shape[1]) + ");"
     
+    dailyData = dailyData[["dataDate","wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"]]
+    #与SQL的空值兼容问题
+    dailyData = dailyData.where(dailyData.notnull(), None)   
     
     try:
-        dailyData = dailyData[["dataDate","wind_code","hold_stocks","publish_ratio","calculate_ratio","float_sharesratio"]]
-        #与SQL的空值兼容问题
-        dailyData = dailyData.where(dailyData.notnull(), None)   
-    
-        try:
-            cursor.execute(sqlCodeDelete)
-            db.commit()
-            cursor.executemany(sqlCodeInsert, dailyData.values.tolist())
-            db.commit()
-            print ("Finish updating daily trading data: %s rows." % str(len(dailyData)))
-        except:
-            db.rollback()
-            print ("Failed")
+        cursor.execute(sqlCodeDelete)
+        db.commit()
+        cursor.executemany(sqlCodeInsert, dailyData.values.tolist())
+        db.commit()
+        print ("Finish updating daily trading data: %s rows." % str(len(dailyData)))
     except:
-        print ("There is no data for date:%s"  % updateDate)
+        db.rollback()
+        print ("Failed")
+
     
-    time.sleep(10)
+    time.sleep(5)
     
 db.close()
 
